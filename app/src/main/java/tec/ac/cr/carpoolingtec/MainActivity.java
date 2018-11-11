14367d5723f0e04611860d0689a47deb097014f0
Kevin Cordero <38987651+Skryfall@users.noreply.github.com>
2018-11-11 17:22:52 -0600

    Añadido Inicio de Sesión con Facebook

--- app/src/main/java/tec/ac/cr/carpoolingtec/MainActivity.java
+++ app/src/main/java/tec/ac/cr/carpoolingtec/MainActivity.java
@@ -1,15 +1,35 @@
 package tec.ac.cr.carpoolingtec;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager = CallbackManager.Factory.create();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +46,39 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(R.string.success);
+            }
+
+            @Override
+            public void onCancel() {
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(R.string.failure);
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(R.string.error);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
---
+++ app/src/main/java/tec/ac/cr/carpoolingtec/test/Test.java
@@ -0,0 +1,11 @@
+package tec.ac.cr.carpoolingtec.test;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+
+public class Test {
+
+
+
+}
