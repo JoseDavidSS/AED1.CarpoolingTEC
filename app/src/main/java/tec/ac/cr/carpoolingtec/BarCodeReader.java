2230ec4e6b80e7d7043b2dd7e7da22208b417115
Kevin Cordero <38987651+Skryfall@users.noreply.github.com>
2018-11-16 15:06:01 -0600

    Añadido el escaner de código de barras (para carnés) 100% funcional, a tiempo real (es necesario darle permiso a la app para usar la cámara).

---
+++ app/src/main/java/tec/ac/cr/carpoolingtec/BarCodeReader.java
@@ -0,0 +1,107 @@
+package tec.ac.cr.carpoolingtec;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Vibrator;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class BarCodeReader extends AppCompatActivity {
+
+    private SurfaceView surfaceView;
+    private CameraSource cameraSource;
+    private TextView textView;
+    private BarcodeDetector barcodeDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_code_reader);
+
+        surfaceView = findViewById(R.id.surfaceView);
+        textView = findViewById(R.id.textView3);
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.CODE_128)
+                .build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .setAutoFocusEnabled(true)
+                .build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                try{
+                    cameraSource.start(holder);
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcode = detections.getDetectedItems();
+
+                if(barcode.size() != 0){
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Vibrator vibrator = (Vibrator)getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                            vibrator.vibrate(1000);
+                            textView.setText(barcode.valueAt(0).displayValue);
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    public void exit(View v){
+        finish();
+    }
+}
---
+++ app/src/main/java/tec/ac/cr/carpoolingtec/LoginActivity.java
@@ -0,0 +1,160 @@
+package tec.ac.cr.carpoolingtec;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.linkedin.platform.APIHelper;
+import com.linkedin.platform.LISessionManager;
+import com.linkedin.platform.errors.LIApiError;
+import com.linkedin.platform.errors.LIAuthError;
+import com.linkedin.platform.listeners.ApiListener;
+import com.linkedin.platform.listeners.ApiResponse;
+import com.linkedin.platform.listeners.AuthListener;
+import com.linkedin.platform.utils.Scope;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private CallbackManager callbackManager = CallbackManager.Factory.create();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        this.facebookLogin();
+        this.initializeControls();
+    }
+
+    private void initializeControls(){
+        ImageView imgLogin = (ImageView)findViewById(R.id.imageView);
+        imgLogin.setOnClickListener(this);
+        Button buttonLogout = (Button)findViewById(R.id.button2);
+        buttonLogout.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        LISessionManager.getInstance(getApplicationContext()).onActivityResult(this, requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void facebookLogin(){
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(R.string.success);
+            }
+
+            @Override
+            public void onCancel() {
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(R.string.failure);
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(R.string.error);
+            }
+        });
+    }
+
+    private void linkedInLogin(){
+        LISessionManager.getInstance(getApplicationContext()).init(this, buildScope(), new AuthListener() {
+            @Override
+            public void onAuthSuccess() {
+                fetchLinkedInInfo();
+            }
+
+            @Override
+            public void onAuthError(LIAuthError error) {
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(R.string.error);
+            }
+        }, true);
+    }
+
+    private void linkedInLogout(){
+        LISessionManager.getInstance(getApplicationContext()).clearSession();
+    }
+
+    private void fetchLinkedInInfo(){
+        String url = "https://api.linkedin.com/v1/people/~:(id,first-name,last-name,public-profile-url,email-address,picture-urls::(original))";
+
+        APIHelper apiHelper = APIHelper.getInstance(getApplicationContext());
+        apiHelper.getRequest(this, url, new ApiListener() {
+            @Override
+            public void onApiSuccess(ApiResponse apiResponse) {
+                try {
+                    JSONObject jsonObject = apiResponse.getResponseDataAsJson();
+                    String firstName = jsonObject.getString("firstName");
+                    String lastName = jsonObject.getString("lastName");
+                    String pictureURL = jsonObject.getString("pictureURL");
+                    String emailAddress = jsonObject.getString("emailAddress");
+
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append("First Name" + firstName);
+                    stringBuilder.append("\n\n");
+                    stringBuilder.append("Last Name" + lastName);
+                    stringBuilder.append("\n\n");
+                    stringBuilder.append("Email" + emailAddress);
+
+                    TextView textView = findViewById(R.id.textView);
+                    textView.setText(stringBuilder);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onApiError(LIApiError liApiError) {
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(R.string.error);
+            }
+        });
+    }
+
+    // Build the list of member permissions our LinkedIn session requires
+    private static Scope buildScope() {
+        return Scope.build(Scope.R_BASICPROFILE, Scope.W_SHARE, Scope.R_EMAILADDRESS);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imageView:
+                linkedInLogin();
+                break;
+            case R.id.button2:
+                linkedInLogout();
+                break;
+        }
+    }
+
+    public void exit(View v){
+        finish();
+    }
+
+}
