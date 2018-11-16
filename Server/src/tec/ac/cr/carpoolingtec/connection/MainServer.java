7322f9f41b4dff00d8d618759d0a23c92cd2e9c1
Apu <jp.1398@hotmail.com>
2018-11-16 17:34:49 -0600

    Configura server

--- Server/src/tec/ac/cr/carpoolingtec/connection/MainServer.java
+++ Server/src/tec/ac/cr/carpoolingtec/connection/MainServer.java
@@ -1,15 +1,15 @@
 package tec.ac.cr.carpoolingtec.connection;
 
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("carpoolingtec")
+@ApplicationPath("hola")
+@Path("hola")
 public class MainServer extends Application {
-
-    @Path("/hola")
     @GET
     public String main() {
         return "Server Configurado";
