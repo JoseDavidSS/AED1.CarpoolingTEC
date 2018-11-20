88e1baab2d92014e45e3b0f66d22dd6a76f36b4f
Alejandro I <josealejandroibarra@hotmail.com>
2018-11-20 11:21:30 -0600

    Now with beautifully crafted points and lines. #progress

--- app/src/main/java/tec/ac/cr/carpoolingtec/MainActivity.java
+++ app/src/main/java/tec/ac/cr/carpoolingtec/MainActivity.java
@@ -39,163 +39,18 @@ import com.linkedin.platform.LISessionManager;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private CallbackManager callbackManager = CallbackManager.Factory.create();
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        this.facebookLogin();
-        this.initializeControls();
     }
 
-    private void initializeControls(){
-        ImageView imgLogin = (ImageView)findViewById(R.id.imageView);
-        imgLogin.setOnClickListener(this);
-        Button buttonLogout = (Button)findViewById(R.id.button2);
-        buttonLogout.setOnClickListener(this);
+    public void goToRiderView(View v){
+        Intent intent = new Intent(this, RiderView.class);
+        startActivity(intent);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        LISessionManager.getInstance(getApplicationContext()).onActivityResult(this, requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-    }
 
-    private void facebookLogin(){
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
-
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("email");
-
-        // Callback registration
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                TextView textView = findViewById(R.id.textView);
-                textView.setText(R.string.success);
-            }
-
-            @Override
-            public void onCancel() {
-                TextView textView = findViewById(R.id.textView);
-                textView.setText(R.string.failure);
-            }
-
-            @Override
-            public void onError(FacebookException exception) {
-                TextView textView = findViewById(R.id.textView);
-                textView.setText(R.string.error);
-            }
-        });
-    }
-
-    private void linkedInLogin(){
-        LISessionManager.getInstance(getApplicationContext()).init(this, buildScope(), new AuthListener() {
-            @Override
-            public void onAuthSuccess() {
-                fetchLinkedInInfo();
-            }
-
-            @Override
-            public void onAuthError(LIAuthError error) {
-                TextView textView = findViewById(R.id.textView);
-                textView.setText(R.string.error);
-            }
-        }, true);
-    }
-
-    private void linkedInLogout(){
-        LISessionManager.getInstance(getApplicationContext()).clearSession();
-    }
-
-    private void fetchLinkedInInfo(){
-        String url = "https://api.linkedin.com/v1/people/~:(id,first-name,last-name,public-profile-url,email-address,picture-urls::(original))";
-
-        APIHelper apiHelper = APIHelper.getInstance(getApplicationContext());
-        apiHelper.getRequest(this, url, new ApiListener() {
-            @Override
-            public void onApiSuccess(ApiResponse apiResponse) {
-                try {
-                    JSONObject jsonObject = apiResponse.getResponseDataAsJson();
-                    String firstName = jsonObject.getString("firstName");
-                    String lastName = jsonObject.getString("lastName");
-                    String pictureURL = jsonObject.getString("pictureURL");
-                    String emailAddress = jsonObject.getString("emailAddress");
-
-                    StringBuilder stringBuilder = new StringBuilder();
-                    stringBuilder.append("First Name" + firstName);
-                    stringBuilder.append("\n\n");
-                    stringBuilder.append("Last Name" + lastName);
-                    stringBuilder.append("\n\n");
-                    stringBuilder.append("Email" + emailAddress);
-
-                    TextView textView = findViewById(R.id.textView);
-                    textView.setText(stringBuilder);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onApiError(LIApiError liApiError) {
-                TextView textView = findViewById(R.id.textView);
-                textView.setText(R.string.error);
-            }
-        });
-    }
-
-    // Build the list of member permissions our LinkedIn session requires
-    private static Scope buildScope() {
-        return Scope.build(Scope.R_BASICPROFILE, Scope.W_SHARE, Scope.R_EMAILADDRESS);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.imageView:
-                linkedInLogin();
-                break;
-            case R.id.button2:
-                linkedInLogout();
-                break;
-        }
-    }
 }
