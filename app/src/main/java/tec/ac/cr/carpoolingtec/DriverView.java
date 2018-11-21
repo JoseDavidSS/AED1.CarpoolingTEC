8268706c1661d7355c9e57c8cbc75b314d6cda89
Alejandro I <josealejandroibarra@hotmail.com>
2018-11-21 11:11:34 -0600

    Now with stunning GUI and brilliant 30 points + lines. #cool Also, this is a merge with relationship-control.

---
+++ app/src/main/java/tec/ac/cr/carpoolingtec/DriverView.java
@@ -0,0 +1,13 @@
+package tec.ac.cr.carpoolingtec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DriverView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_view);
+    }
+}
